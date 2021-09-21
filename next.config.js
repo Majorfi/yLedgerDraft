37c8444c9f6e90c757ad1fbc0737d6b19df0e7b5
Major-Eth <major-tom.eth@pm.me>
2021-09-21 20:14:24 +0200

    ✨ Working with onboardjs

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const Dotenv = require('dotenv-webpack');
+
+module.exports = ({
+	plugins: [
+		new Dotenv()
+	],
+	env: {
+		ONBOARD_KEY: process.env.ONBOARD_KEY,
+		INFURA_KEY: process.env.INFURA_KEY,
+	}
+});
--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,42 @@
-import React, {useEffect, useState} from 'react';
-import { IFrameEthereumProvider } from '@ledgerhq/iframe-provider';
-import {ethers} from 'ethers';
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import  Web3  from 'web3';
-import Onboard from 'bnc-onboard'
+import	React, {useEffect, useState}	from	'react';
+import	{IFrameEthereumProvider}		from	'@ledgerhq/iframe-provider';
+import	{ethers}						from	'ethers';
+import	Head							from	'next/head';
+import	Web3 							from	'web3';
+import	Onboard							from	'bnc-onboard';
 
+// eslint-disable-next-line no-unused-vars
 let web3;
 const wallets = [
-  { walletName: "metamask", preferred: true },
-  {
-    walletName: 'ledger',
-    rpcUrl: 'https://eth-mainnet.alchemyapi.io/v2/nfftXnqpidrTTkFW1AbSJYqz18YiIpqZ',
-    preferred: true,
-    LedgerTransport: "44'/60'/0'/0"
-  },
-  { walletName: "frame", preferred: true },
-]
+	{walletName: 'metamask', preferred: true},
+	{
+		walletName: 'ledger',
+		rpcUrl: `https://eth-mainnet.alchemyapi.io/v2/${process.env.INFURA_KEY}`,
+		preferred: true,
+	},
+	{walletName: 'frame', preferred: true},
+];
 
 const walletChecks = [
-  { checkName: 'derivationPath' },
-  { checkName: 'accounts' },
-  { checkName: 'connect' },
-  { checkName: 'network' },
-  { checkName: 'balance' }
-]
+	{checkName: 'derivationPath'},
+	{checkName: 'accounts'},
+	{checkName: 'connect'},
+	{checkName: 'network'},
+	{checkName: 'balance'}
+];
 
 
 const onboard = Onboard({
-  dappId: 'f7c91b29-1bd0-496d-88b1-99034d104563',       // [String] The API key created by step one above
-  networkId: 1,
-  walletSelect: {wallets: wallets},
-  walletCheck: walletChecks,
-  subscriptions: {
-    wallet: wallet => {
-       web3 = new Web3(wallet.provider)
-    }
-  }
+	dappId: process.env.ONBOARD_KEY,
+	networkId: 1,
+	walletSelect: {wallets: wallets},
+	walletCheck: walletChecks,
+	subscriptions: {
+		wallet: wallet => {
+			web3 = new Web3(wallet.provider);
+		}
+	}
 });
-
 
 
 const  ETH_VAULT = '0xa258C4606Ca8206D8aA700cE2143D7db854D168c';
@@ -92,74 +90,83 @@ async function	apeInVault({provider, contractAddress, amount}, callback) {
 }
 
 export default function Home() {
-  const [isIframe, set_isIframe] = useState(false)
-  const [ethereum, set_ethereum] = useState(undefined)
-  const [provider, set_provider] = useState(undefined)
-  const [balance, set_balance] = useState(0)
+	const [isIframe, set_isIframe] = useState(false);
+	const [ethereum, set_ethereum] = useState(undefined);
+	const [provider, set_provider] = useState(undefined);
+	const [balance, set_balance] = useState(0);
 
-  async function onBoard() {
-    await onboard.walletSelect();
-    await onboard.walletCheck();
-    const currentState = onboard.getState()
-    console.log(currentState)
-  }
+	async function onBoard() {
+		let	stepSuccess = await onboard.walletSelect();
+		if (stepSuccess) {
+			stepSuccess = await onboard.walletCheck();
+			if (stepSuccess) {
+				const currentState = onboard.getState();
+				if (currentState) {
+					set_balance(ethers.utils.formatEther(currentState.balance));
+				}
+			}
+		}
+	}
 
-  useEffect(() => {
-    if (window.location.href.includes('embed=true')) {
-      set_isIframe(true);
-      set_ethereum(new IFrameEthereumProvider());
-    } else {
-      onBoard()
-    }
-  }, [typeof(window) !== 'undefined'])
+	useEffect(() => {
+		if (window.location.href.includes('embed=true')) {
+			set_isIframe(true);
+			set_ethereum(new IFrameEthereumProvider());
+		} else {
+			onBoard();
+		}
+	}, [typeof(window) !== 'undefined']);
 
-  useEffect(() => {
-    if (ethereum) {
-      const web3Provider = new ethers.providers.Web3Provider(ethereum);
-      set_provider(web3Provider);
+	useEffect(() => {
+		if (ethereum) {
+			const web3Provider = new ethers.providers.Web3Provider(ethereum);
+			set_provider(web3Provider);
 
-      const signer = web3Provider.getSigner()
-      signer.getBalance().then(e => set_balance(ethers.utils.formatEther(e)))
-    }
-  }, [ethereum])
+			const signer = web3Provider.getSigner();
+			signer.getBalance().then(e => set_balance(ethers.utils.formatEther(e)));
+		}
+	}, [ethereum]);
 
-  if (isIframe) {
-    ethereum?.on('accountsChanged', function (accounts) {
-      if (provider) {
-        const signer = provider.getSigner()
-        signer.getBalance().then(e => set_balance(ethers.utils.formatEther(e)))
-        console.warn(accounts)
-      }
-    })
-  }
+	if (isIframe) {
+		ethereum?.on('accountsChanged', function (accounts) {
+			if (provider) {
+				const signer = provider.getSigner();
+				signer.getBalance().then(e => set_balance(ethers.utils.formatEther(e)));
+				console.warn(accounts);
+			}
+		});
+	}
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>yLedger</title>
-        <meta name="description" content="yLedger" />
-      </Head>
+	return (
+		<div className={'container'}>
+			<Head>
+				<title>{'yLedger'}</title>
+				<meta name={'description'} content={'yLedger'} />
+			</Head>
 
-      <main className={styles.main}>
-        <button>
-          {'CONNECT'}
-        </button>
+			<main className={'main'}>
+				<button onClick={onBoard}>
+					{'CONNECT'}
+				</button>
 
-        <h1 className={styles.title}>
-          Balance: <a href="#">{balance}</a>
-        </h1>
+				<h1 className={'title'} style={{marginTop: '4rem', marginBottom: '4rem'}}>
+					{'Balance: '}<a href={'#'}>{balance}</a>
+				</h1>
 
-        <button onClick={() => approveVault({provider, contractAddress: ETH_VAULT, amount: 1})}>
-          {'APPROVE'}
-        </button>
-        <button onClick={() => apeInVault({provider, contractAddress: ETH_VAULT, amount: 1})}>
-          {'DEPOSIT'}
-        </button>
-        <button onClick={() => alert('nop')}>
-          {'WITHDRAW'}
-        </button>
-
-      </main>
-    </div>
-  )
+				<div style={{display: 'flex'}}>
+					<button onClick={() => approveVault({provider, contractAddress: ETH_VAULT, amount: 1})}>
+						{'APPROVE'}
+					</button>
+					<button
+						style={{marginLeft: '2rem', marginRight: '2rem'}}
+						onClick={() => apeInVault({provider, contractAddress: ETH_VAULT, amount: 1})}>
+						{'DEPOSIT'}
+					</button>
+					<button onClick={() => alert('nop')}>
+						{'WITHDRAW'}
+					</button>
+				</div>
+			</main>
+		</div>
+	);
 }
